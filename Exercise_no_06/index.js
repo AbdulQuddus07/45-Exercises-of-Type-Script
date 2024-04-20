cedf1c50f8da09edd306e632e2714ef82968e9d9
Abdul Quddus <abdulquddus33kk@gmail.com>
2024-04-20 23:48:48 +0500

    Exercise of "Famous Quote 2" and "Stripping Names"

---
+++ Exercise_no_06/index.js
@@ -0,0 +1,8 @@
+//  Q:06 Stripping Names: Store a person’s name, and include some whitespace characters
+// at the beginning and end of the name. Make sure you use each character combination,
+// "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed.
+// Then print the name after striping the white spaces.
+var whitespaceName = "\n\t Abdul Quddus \t\n";
+console.log(whitespaceName);
+var withoutWhiteSpace = whitespaceName.trim();
+console.log(withoutWhiteSpace);
