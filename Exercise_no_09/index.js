0c2df4883d907607f10468d1773b87308af9549c
Abdul Quddus <abdulquddus33kk@gmail.com>
2024-04-30 23:42:51 +0500

    "new task done"

--- Exercise_no_09/index.js
+++ Exercise_no_09/index.js
@@ -1,6 +1,5 @@
 //  Q:09 Favorite Number: Store your favorite number in a variable. Then, using that variable,
 // create a message that reveals your favorite number. Print that message.
-
-let favoriteNumber = 7;
-let message = "My Favorite Number is favoriteNumber:";
-console.log(message ,favoriteNumber);
+var favoriteNumber = 7;
+var message = "My Favorite Number is favoriteNumber:";
+console.log(message, favoriteNumber);
