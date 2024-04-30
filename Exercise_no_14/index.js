442813eaacf206959c5426fd38d193c8101d318f
Abdul Quddus <abdulquddus33kk@gmail.com>
2024-04-30 23:45:34 +0500

    "new exercise"

---
+++ Exercise_no_14/index.js
@@ -0,0 +1,2 @@
+var guestList = ["Hamza", "Hunain", "Hamid", "Haider"];
+guestList.forEach(function (wellcome) { return console.log("Assalam-o-Alaikum ".concat(wellcome, " would you like to dinner to dinner with me")); });
---
+++ Exercise_no_14/index.ts
@@ -0,0 +1,3 @@
+let guestList = ["Hamza", "Hunain", "Hamid", "Haider"];
+guestList.forEach(wellcome => console.log(`Assalam-o-Alaikum ${wellcome}, would you like to dinner to dinner with me`)
+);
