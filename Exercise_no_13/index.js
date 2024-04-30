1139fe706b39f3ae14667655706d18382b5bafad
Abdul Quddus <abdulquddus33kk@gmail.com>
2024-04-30 23:44:21 +0500

    Done-TS

---
+++ Exercise_no_13/index.js
@@ -0,0 +1,2 @@
+var transportationMode = ["Sport Bike", "Bus", "Car", "Sport Car", "Train", "Aeroplane"];
+transportationMode.forEach(function (mode) { return console.log("I Would like to own a ".concat(mode)); });
---
+++ Exercise_no_13/index.ts
@@ -0,0 +1,3 @@
+let transportationMode = ["Sport Bike", "Bus", "Car", "Sport Car", "Train", "Aeroplane"];
+transportationMode.forEach(mode => console.log(`I Would like to own a ${mode}`)
+);
