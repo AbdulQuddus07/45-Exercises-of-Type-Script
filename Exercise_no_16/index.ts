652b4f597cc575f5fc8c67e08d581eba23bf2075
Abdul Quddus <abdulquddus33kk@gmail.com>
2024-05-06 23:23:07 +0500

    "new_task"

---
+++ Exercise_no_16/index.ts
@@ -0,0 +1,20 @@
+// Q:16 You just found a bigger dinner table, so now more space is available.
+// Think of three more guests to invite to dinner.Start with your program from Exercise 15.
+// Add a print statement to the end of your program informing people that you found a bigger dinner table.
+// Add one new guest to the beginning of your array.Add one new guest to the middle of your array. •
+// Use append() to add one new guest to the end of your list. • Print a new set of invitation messages,
+// one for each person in your list.
+
+var guest_List = ["Hamza", "Hunain", "Hamid", "Haider"];
+
+ // adding new member in last index 
+guest_List.push("Abdul Quddus");
+console.log(guest_List);
+
+ // adding new member in first index 
+guest_List.unshift("Abdul Quddus");
+console.log(guest_List);
+
+ // adding new member in middle index
+guest_List.splice(3,0,"Abdul Quddus");
+ console.log(guest_List);
